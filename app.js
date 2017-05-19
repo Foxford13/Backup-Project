17b7601b753a64334b7e4b65de9f15868dafe01e
Jakub Dziekan <jdziek89@gmail.com>
2017-05-19 16:46:56 +0100

    im going insane

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(() => {
   const $boxes = $('.box');
   const $isPlaying = $('.current');
   let playerOnePlaying = true;
-  var myArray = new Array();
+  // var myArray = new Array();
 
   $isPlaying.text('Player 1');
 
@@ -13,41 +13,69 @@ $(() => {
 
   });
 
-  $boxes.each(arrayOfSquares);
+  // $boxes.each(arrayOfSquares);
 
-  function arrayOfSquares(index, box) {
-    if ($(box).hasClass('black')) {
-      myArray.push('black');
-    }else if ($(box).hasClass('yellow')) {
-      myArray.push('yellow');
-    } else {
-      myArray.push(undefined);
-    }
-  }
+  // function arrayOfSquares(index, box) {
+  //   if ($(box).hasClass('black')) {
+  //     myArray.push('black');
+  //   }else if ($(box).hasClass('yellow')) {
+  //     myArray.push('yellow');
+  //   } else {
+  //     myArray.push(undefined);
+  //   }
+  //
+  // }
+  // function boxAvailabilityAndFlip() {
+  //   // horizontal for now
+  //
+  // }
+
+
+
 
   function alternateMatches(e){
-    myArray = new Array();
+    // myArray = new Array();
+
+    ////// here i can add a function
     if(playerOnePlaying){
       $(e.target).addClass('black');
       playerOnePlaying = false;
       $isPlaying.text('Player 2');
-      console.log(playerOnePlaying);
+
     } else if(!playerOnePlaying){
       $(e.target).addClass('yellow');
       playerOnePlaying = true;
       console.log(playerOnePlaying);
       $isPlaying.text('Player 1');
     }
-    $boxes.each(arrayOfSquares);
-    console.log(myArray);
+    // $boxes.each(arrayOfSquares);
+    // console.log(myArray);
   }
+  //remember about while loop
+
+
   $boxes.one('click', alternateMatches);
 
-  $boxes.on('click', (e)=> {
-    console.log('yay');
+  $boxes.one('click', (e) => {
+    let index = $(e.target).index();
+    index += 1;
+    if ($boxes.eq(index).hasClass('yellow')) {
+      $('body').find($boxes).eq(index).removeClass('yellow').addClass('black');
+    }
+
+
+
   });
+  $boxes.one('click', (e) => {
+    let index = $(e.target).index();
+    index += 1;
+    if ($boxes.eq(index).hasClass('black')) {
+      $('body').find($boxes).eq(index).removeClass('black').addClass('yellow');
+    }
 
 
+
+  });
 
 
 
