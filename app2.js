1d8020804f3e6b799f6ee77afec3da9c42db7c80
Jakub Dziekan <jdziek89@gmail.com>
2017-05-23 11:13:00 +0100

    i hope it doesnt cross the borders now

--- app2.js
+++ app2.js
@@ -1,93 +1,106 @@
 $(() => {
 
 
-  const $boxes = $('.box');
+
   const $isPlaying = $('.current');
   let playerOnePlaying = true;
+  const boardSize = 64;
   $isPlaying.text('Player 1 - Grey');
-  $('.box').each(function(i) {  //////// adds numbers to each box
-    $(this).text(i);
-  });
 
-  // function fillBoxesYellow(e) {
-  //   const index = $(e.target).index();
-  //   let squaresToTurn = [];
-  //   let i = 1;
-  //   while ($boxes.eq(index + i).hasClass('yellow')) {
-  //     squaresToTurn.push(index + i);
-  //     console.log(squaresToTurn, 'first');
-  //     i += 1;
-  //     console.log($boxes.eq(squaresToTurn[squaresToTurn.length - 1] + 1));
-  //   }
-  //   if ( $boxes.eq(squaresToTurn[squaresToTurn.length - 1] + 1).hasClass('black')) {
-  //     for (let i = 0; i < squaresToTurn.length; i++) {
-  //       $(e.target).addClass('black');
-  //       $('body').find($boxes).eq(squaresToTurn[i]).removeClass('yellow').addClass('black');
-  //       console.log(squaresToTurn, 'second');
-  //     }
-  //   }
-  // }
+
+  function startingPosition () {
+
+    for ( let i = 0; i < boardSize; i++) {
+      $('.game').append($('<div>').addClass('box'));
+    }
+    $('.box').each(function(i) {  //////// adds numbers to each box
+      $(this).text(i);
+    });
+    $('body').find($('.box')).eq(27).addClass('black');
+    $('body').find($('.box')).eq(28).addClass('yellow');
+    $('body').find($('.box')).eq(35).addClass('yellow');
+    $('body').find($('.box')).eq(36).addClass('black');
+
+  }
+  startingPosition();
+
+  const $boxes = $('.box');
+
+
+
+
   function fillBoxes(e, classToAdd, classToRemove, boxNumbers) {
     const index = $(e.target).index();
-    let squaresToTurn = [];
+    const squaresToTurn = [];
     let i = boxNumbers;
+    ///////////////////// it determines the the color  of boxes based on possible coordinates given and
+    ///////////////////// puts them into an array ex. for forward  can  have 4 yellow squares ahead(+1 )
+    ////////////////////    or for diaganal up     three yellows -7 while they are still there
     while ($boxes.eq(index + i).hasClass(classToRemove)) {
       squaresToTurn.push(index + i);
-      console.log(squaresToTurn, 'first');
       i += boxNumbers;
-      console.log($boxes.eq(squaresToTurn[squaresToTurn.length - 1] + boxNumbers));
     }
-    const lastFilledDivInASequence = squaresToTurn[squaresToTurn.length - 1] + 1;
-    if ( $boxes.eq(squaresToTurn[squaresToTurn.length - 1] + boxNumbers).hasClass(classToAdd) && lastFilledDivInASequence >= 0 && lastFilledDivInASequence <= 63) {
-      console.log(lastFilledDivInASequence);
+    ////////////  establishes a class of a checker after the sequence of checkers that have a class we want to
+    // remove
+    const lastFilledDivInASequence = squaresToTurn[squaresToTurn.length - 1] + boxNumbers;
+    console.log(lastFilledDivInASequence);
+
+    //////////// if
+
+    if ( $boxes.eq(lastFilledDivInASequence).hasClass(classToAdd) && lastFilledDivInASequence >= 0 && lastFilledDivInASequence <= 63) {
+
+
       for (let i = 0; i < squaresToTurn.length; i++) {
         $(e.target).addClass(classToAdd);
         $('body').find($boxes).eq(squaresToTurn[i]).removeClass(classToRemove).addClass(classToAdd);
-        console.log(squaresToTurn, 'second');
+
         playerOnePlaying = classToAdd === 'black' ? false : true;
-        const player = playerOnePlaying ? $isPlaying.text('Player 1 - Grey') : $isPlaying.text('Player 2 - Yellow');
+        playerOnePlaying ? $isPlaying.text('Player 1 - Grey') : $isPlaying.text('Player 2 - Yellow');
       }
     }
   }
 
-  $boxes.one('click', (e) => {
-    ///replacing yelow squares with black square
-    // const emptyBoxes = (!($(e.target).hasClass('yellow')) && !($(e.target).hasClass('black')));
-    // if(playerOnePlaying && (!($(e.target).hasClass('yellow')) && !($(e.target).hasClass('black')))){
+  $boxes.on('click', (e) => {
+    const $indexTarget = $(e.target).index();
+    console.log($(e.target).index());
 
-    if(playerOnePlaying) {
-      fillBoxes(e, 'black', 'yellow', +1);
-      fillBoxes(e, 'black', 'yellow', -1);
-
-      fillBoxes(e, 'black', 'yellow', +8);
-      fillBoxes(e, 'black', 'yellow', -8);
-
-      fillBoxes(e, 'black', 'yellow', +9);
-      fillBoxes(e, 'black', 'yellow', -9);
-
-      fillBoxes(e, 'black', 'yellow', +7);
-      fillBoxes(e, 'black', 'yellow', -7);
-
-    } else{
-
-      fillBoxes(e, 'yellow', 'black', +1);
-      fillBoxes(e, 'yellow', 'black', -1);
-
-      fillBoxes(e, 'yellow', 'black', +8);
-      fillBoxes(e, 'yellow', 'black', -8);
-
-      fillBoxes(e, 'yellow', 'black', +9);
-      fillBoxes(e, 'yellow', 'black', -9);
-
-      fillBoxes(e, 'yellow', 'black', +7);
-      fillBoxes(e, 'yellow', 'black', -7);
-
-      playerOnePlaying = true;
+    function blackPlus() {
+      if (playerOnePlaying && !($indexTarget % 8 === 7)) {
+        fillBoxes(e, 'black', 'yellow', +1);
+        fillBoxes(e, 'black', 'yellow', +8);
+        fillBoxes(e, 'black', 'yellow', +9);
+        fillBoxes(e, 'black', 'yellow', +7);
+      }
     }
-    // }else if(!playerOnePlaying && (!($(e.target).hasClass('yellow')) && !($(e.target).hasClass('black')))){
+    blackPlus();
+    function blackMinus() {
+      if (playerOnePlaying && !($indexTarget % 8 === 0)){
+        fillBoxes(e, 'black', 'yellow', -1);
+        fillBoxes(e, 'black', 'yellow', -8);
+        fillBoxes(e, 'black', 'yellow', -9);
+        fillBoxes(e, 'black', 'yellow', -7);
+      }
+    }
+    blackMinus();
+    function yellowPlus() {
+      if (!playerOnePlaying && !($indexTarget % 8 === 7)){
+        fillBoxes(e, 'yellow', 'black', +1);
+        fillBoxes(e, 'yellow', 'black', +8);
+        fillBoxes(e, 'yellow', 'black', +9);
+        fillBoxes(e, 'yellow', 'black', +7);
+      }
+    }
+    yellowPlus();
+    function yellowMinus() {
+      if (!playerOnePlaying && !($indexTarget % 8 === 0)){
+        fillBoxes(e, 'yellow', 'black', -1);
+        fillBoxes(e, 'yellow', 'black', -8);
+        fillBoxes(e, 'yellow', 'black', -9);
+        fillBoxes(e, 'yellow', 'black', -7);
+      }
+    }
+    yellowMinus();
 
-
-    //// replacing black square with yellow squar
 
   });
 
